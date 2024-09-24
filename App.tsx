601954f8d4420e6ba0ab9b57cc4cd4a2d548e7b3
Ian <iancunha20@gmail.com>
2024-09-24 01:49:09 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = () => {
+    setCount((number) => number + 1)
+  }
+
+  const decrementCount = () => {
+    setCount((number) => number - 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.counter}>{count}</Text>
+
+      <View style={styles.containerButtons}>
+        <Button title='Remover' onPress={decrementCount} />
+        <Button title='Adicionar' onPress={incrementCount} />
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +29,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  counter: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: 'white',
+    paddingVertical: 10,
+  },
+  containerButtons: {
+    display: 'flex',
+    flexDirection: 'row',
   },
 });
